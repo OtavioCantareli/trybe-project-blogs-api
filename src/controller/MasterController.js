4b19a113fe6fd3ad2005c2a4eed9332434b64288
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-13 18:24:27 -0300

    feat: Req 04, and refactor 03
    Refactored middlewares of req03, and finished req04 (test shows as req10 :/)

--- src/controller/MasterController.js
+++ src/controller/MasterController.js
@@ -10,27 +10,83 @@ const router = express.Router();
 
 const secret = process.env.JWT_SECRET;
 
-router.post('/login', async (req, res) => {
+const validateLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: 'Some required fields are missing',
+    });
+  }
+
+  const user = await User.findOne({ where: { email, password } });
+
+  if (!user) {
+    return res.status(400).json({
+      message: 'Invalid fields',
+    });
+  }
+
+  return next();
+};
+
+router.post('/login', validateLogin, async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({
-        message: 'Some required fields are missing',
-      });
-    }
-
     const user = await User.findOne({ where: { email, password } });
-
-    if (!user) {
-      return res.status(400).json({
-        message: 'Invalid fields',
-      });
-    }
 
     const token = jwt.sign({ data: user }, secret);
 
     return res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+const validatePost = async (req, res, next) => {
+  const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+  const { displayName, email, password } = req.body;
+
+  if (displayName.length < 8) {
+    return res.status(400).json({
+      message: '"displayName" length must be at least 8 characters long',
+    });
+  }
+
+  if (!regex.test(email)) {
+    return res.status(400).json({
+      message: '"email" must be a valid email',
+    });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: '"password" length must be at least 6 characters long',
+    });
+  }
+
+  return next();
+};
+
+router.post('/user', validatePost, async (req, res) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+
+    const exists = await User.findOne({ where: { email } });
+
+    if (exists) {
+      return res.status(409).json({
+        message: 'User already registered',
+      });
+    }
+
+    const user = await User.create({ displayName, email, password, image });
+
+    const token = jwt.sign({ data: user }, secret);
+
+    return res.status(201).json({ token });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
