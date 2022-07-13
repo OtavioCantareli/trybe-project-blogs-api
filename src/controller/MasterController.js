fb00993031d9e3ef780ca0f9a008e9f8b1c8eefc
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-13 18:46:24 -0300

    chore: Token validation
    Added token validation

--- src/controller/MasterController.js
+++ src/controller/MasterController.js
@@ -10,6 +10,24 @@ const router = express.Router();
 
 const secret = process.env.JWT_SECRET;
 
+const validateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token not found',
+    });
+  }
+
+  if (!jwt.verify(token, secret)) {
+    return res.status(401).json({
+      message: 'Expired or invalid token',
+    });
+  }
+
+  return next();
+};
+
 const validateLogin = async (req, res, next) => {
   const { email, password } = req.body;
 
