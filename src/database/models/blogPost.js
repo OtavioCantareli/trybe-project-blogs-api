92771902fb47f71f13a64a6cda8fe536da0c1995
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-14 12:19:01 -0300

    chore: Modification
    Added AUTO INCREMENT and PRIMARY KEY fields

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -2,7 +2,7 @@ const BlogPost = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define(
     "BlogPost",
     {
-      id: { type: DataTypes.INTEGER, primaryKey: true },
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement:true },
       title: { type: DataTypes.STRING, allowNull: false },
       content: { type: DataTypes.STRING, allowNull: false },
       userId: { type: DataTypes.INTEGER, foreignKey: true },
