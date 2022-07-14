ed54d0db59f8f4ee0df6e97f3b177ec130aafdc6
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-14 10:16:00 -0300

    feat: req10
    Created model for BlogPosts, and added association with users

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,24 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    "BlogPost",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      title: { type: DataTypes.STRING, allowNull: false },
+      content: { type: DataTypes.STRING, allowNull: false },
+      userId: { type: DataTypes.INTEGER, foreignKey: true },
+      published: { type: DataTypes.DATE, allowNull: false },
+      updated: { type: DataTypes.DATE, allowNull: false },
+    },
+    {
+      timestamps: false,
+    }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreignKey: "id", as: "users" });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,13 +1,21 @@
 const User = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    id: { type: DataTypes.INTEGER, primaryKey: true },
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING,
-  }, {
-    timestamps: false
-});
+  const User = sequelize.define(
+    "User",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+    }
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: "id", as: "posts" });
+  };
 
   return User;
 };
